7eaca58bc6064586619db8d2d55c4a916143271f
Victor <victorarroyavecastaneda@gmail.com>
2023-06-05 21:53:03 -0500

    Conections section trending and section category movies used API the movie db

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,62 @@
-console.log(api_key);
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('.scroll-container');
+
+        const movieContainer = document.createElement('article');
+        movieContainer.classList.add('cards-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('trending-image');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        const movieName = document.createElement('h3');
+        movieName.textContent = movie.title;
+
+        const starImage = document.createElement('img');
+        starImage.setAttribute('src', './src/images/logos/Star.svg');
+
+        const calificationMovieContainer = document.createElement('div');
+        calificationMovieContainer.classList.add('new-movie-star');
+
+        const calificationTrendingMovie = document.createElement('p');
+        calificationTrendingMovie.textContent = movie.vote_average;
+
+        movieContainer.appendChild(movieImg);
+        movieContainer.appendChild(movieName);
+        calificationMovieContainer.appendChild(starImage);
+        calificationMovieContainer.appendChild(calificationTrendingMovie);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+        movieContainer.appendChild(calificationMovieContainer);
+    });
+
+}
+
+async function getCategoryMovies() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+    console.log(data);
+
+    const genres = data.genres;
+
+    const genresNamesContainer = document.querySelector('.main__section-opcion-movies');
+    const listGenresContainer = document.createElement('ul');
+
+    genres.forEach(genre => {
+        const itemListGenres = document.createElement('li');
+        const nameGenre = document.createElement('a');
+        nameGenre.textContent = genre.name;
+
+        itemListGenres.appendChild(nameGenre);
+        listGenresContainer.appendChild(itemListGenres);
+    });
+
+    genresNamesContainer.appendChild(listGenresContainer);
+}
+
+getTrendingMoviesPreview();
+getCategoryMovies();
